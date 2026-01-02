9942ed213514afe00d7a6ba4104263dd9fdb1d69
Antony Baasandorj <antony.baasan@gmail.com>
2026-01-01 20:00:53 -0500

    add typer

--- src/main.py
+++ src/main.py
@@ -1,24 +1,23 @@
 # This is a sample Python script.
+import typer
+
 from service.executor_service import executor_service
 from model.execution_request import ExecutionRequest
 
+app = typer.Typer()
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+@app.command()
+def execute(command: str):
+    request = ExecutionRequest(command=command)
+    result = executor_service.execute(request)
+    print(result)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+@app.command()
+def echo(text: str):
+    print(f"Echo result: {text}")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-    request = ExecutionRequest
-    request.command = "Hello world"
-    result = executor_service.execute(request)
-    print(result)
+    app()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
